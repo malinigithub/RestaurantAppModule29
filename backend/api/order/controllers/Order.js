2eb63271884712ab6062512ae434b601fae6c8e1
malinigithub <maliniradha2020@gmail.com>
2023-02-25 16:35:44 -0500

    Restaurant App

---
+++ backend/api/order/controllers/Order.js
@@ -0,0 +1,47 @@
+"use strict";
+
+/**
+ * Order.js controller
+ *
+ * @description: A set of functions called "actions" for managing `Order`.
+ */
+
+const stripe = require("stripe")(
+  "sk_test_51MeT26DxW2Pd3FvuR4I7bJazvdYyEbWjlXLq32xvfUY4MacIH5CJS4OruNs0r95OFWAUlBTQ4ezk1NFr52xmkUaV00RZbQcE7O"
+);
+
+module.exports = {
+  /**
+   * Create a/an order record.
+   *
+   * @return {Object}
+   */
+
+  create: async (ctx) => {
+    const { address, amount, dishes, token, city, state } = JSON.parse(
+      ctx.request.body
+    );
+    const stripeAmount = Math.floor(amount * 100);
+    // charge on stripe
+    const charge = await stripe.charges.create({
+      // Transform cents to dollars.
+      amount: stripeAmount,
+      currency: "usd",
+      description: `Order ${new Date()} by ${ctx.state.user._id}`,
+      source: token,
+    });
+
+    // Register the order in the database
+    const order = await strapi.services.order.create({
+      user: ctx.state.user.id,
+      charge_id: charge.id,
+      amount: stripeAmount,
+      address,
+      dishes,
+      city,
+      state,
+    });
+
+    return order;
+  },
+};
